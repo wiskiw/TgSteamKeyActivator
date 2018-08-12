49679a9d36662824dbb49f571c36020db58a4a28
wiskiw <dron2310@gmail.com>
2018-08-12 18:07:49 +0300

    Update .gitignore

--- node_modules/mtproto-logger/src/index.js
+++
@@ -1,9 +0,0 @@
-//@flow
-
-import './formatter'
-import Logger from './logger'
-
-export { setLogger } from './debug'
-export { logStream } from './sheduler'
-export { Logger }
-export default Logger
